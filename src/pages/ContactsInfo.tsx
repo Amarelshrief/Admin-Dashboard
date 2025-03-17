302f7c32eb67cfbe0092671f06841d98e292065e
Amarelshrief <amarelshrief511@gmail.com>
2025-03-18 01:35:31 +0200

    Add "ContactsData.tsx" component to "ContactsInfo.tsx" page

--- src/pages/ContactsInfo.tsx
+++ src/pages/ContactsInfo.tsx
@@ -1,5 +1,10 @@
-function ContactsInfoPage() {
-  return <div>ContactsInfoPage</div>;
-}
+import ContactsData from "@/components/custom/Contacts/ContactsData";
 
+function ContactsInfoPage() {
+  return (
+    <div>
+      <ContactsData />
+    </div>
+  );
+}
 export default ContactsInfoPage;
