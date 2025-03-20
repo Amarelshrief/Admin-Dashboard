ef9c32f94d4341be9cafe783a2f4803b500c73a7
Amarelshrief <amarelshrief511@gmail.com>
2025-03-21 01:52:27 +0200

    Create Dummy data at "PieData.ts"

---
+++ src/components/custom/Dashboard/PieData.ts
@@ -0,0 +1,55 @@
+export const data_1 = [
+  { browser: "chrome", visitors: 275, fill: "var(--chart-1)" },
+];
+
+export const data_2 = [
+  { browser: "safari", visitors: 200, fill: "var(--chart-2)" },
+];
+
+export const data_3 = [
+  { browser: "firefox", visitors: 287, fill: "var(--chart-3)" },
+];
+
+export const data_4 = [
+  { browser: "edge", visitors: 173, fill: "var(--chart-4)" },
+];
+
+export const config_1 = {
+  browser: {
+    label: "Browser",
+    color: "hsl(var(--chart-1))",
+  },
+  visitors: {
+    label: "Visitors",
+  },
+};
+
+export const config_2 = {
+  browser: {
+    label: "Browser",
+    color: "hsl(var(--chart-2))",
+  },
+  visitors: {
+    label: "Visitors",
+  },
+};
+
+export const config_3 = {
+  browser: {
+    label: "Browser",
+    color: "hsl(var(--chart-3))",
+  },
+  visitors: {
+    label: "Visitors",
+  },
+};
+
+export const config_4 = {
+  browser: {
+    label: "Browser",
+    color: "hsl(var(--chart-4))",
+  },
+  visitors: {
+    label: "Visitors",
+  },
+};
