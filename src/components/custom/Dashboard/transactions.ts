cad515ec2770ba9c90858ecd98411080cc1eb40b
Amarelshrief <amarelshrief511@gmail.com>
2025-03-22 03:42:14 +0200

    Create "transactions.ts" file

---
+++ src/components/custom/Dashboard/transactions.ts
@@ -0,0 +1,62 @@
+export const Transactions = [
+  {
+    user: "John Doe",
+    txId: "TX-123456789",
+    date: "2023-01-01",
+    cost: "$100",
+  },
+  {
+    user: "Jane Doe",
+    txId: "TX-987654321",
+    date: "2023-01-02",
+    cost: "$200",
+  },
+  {
+    user: "John Doe",
+    txId: "TX-123456789",
+    date: "2023-01-03",
+    cost: "$300",
+  },
+  {
+    user: "Jane Doe",
+    txId: "TX-987654321",
+    date: "2023-01-04",
+    cost: "$400",
+  },
+  {
+    user: "John Doe",
+    txId: "TX-123456789",
+    date: "2023-01-05",
+    cost: "$500",
+  },
+  {
+    user: "Jane Doe",
+    txId: "TX-987654321",
+    date: "2023-01-06",
+    cost: "$600",
+  },
+  {
+    user: "John Doe",
+    txId: "TX-123456789",
+    date: "2023-01-07",
+    cost: "$700",
+  },
+  {
+    user: "Jane Doe",
+    txId: "TX-987654321",
+    date: "2023-01-08",
+    cost: "$800",
+  },
+  {
+    user: "John Doe",
+    txId: "TX-123456789",
+    date: "2023-01-09",
+    cost: "$900",
+  },
+  {
+    user: "Jane Doe",
+    txId: "TX-987654321",
+    date: "2023-01-10",
+    cost: "$1000",
+  },
+];
