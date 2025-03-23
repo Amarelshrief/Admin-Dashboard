6abe890dce45c4159cc36971a8c65934f90aa3bc
Amarelshrief <amarelshrief511@gmail.com>
2025-03-24 01:31:34 +0200

    A little of changes

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -58,7 +58,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "data-[state=selected]:bg-muted border-b transition-colors hover:bg-gray-200 duration-150 ",
+        "data-[state=selected]:bg-muted border-b transition-colors hover:bg-[var(--ring)] duration-150 ",
         className
       )}
       {...props}
