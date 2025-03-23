8d7913000a9f3bcac93e420a2d482a952261f0bd
Amarelshrief <amarelshrief511@gmail.com>
2025-03-24 01:35:13 +0200

    Wrap the project by the <ThemeProvider" to handle dark mode

--- src/pages/DashboardRoot.tsx
+++ src/pages/DashboardRoot.tsx
@@ -3,27 +3,30 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 import SidebarPage from "@/components/custom/Sidebar/Sidebar";
 import Header from "@/components/custom/Header/Header";
+import { ThemeProvider } from "@/components/custom/Theme/Theme";
 
 export default function DashboardRootLayout() {
   return (
     <section className="">
-      <div className="flex justify-between items-start h-[4rem] w-full pt-2 shadow-xl">
-        <div>
-          <SidebarProvider>
-            <div className="flex items-start">
-              <SidebarPage />
-              <SidebarTrigger className="-mr-4 cursor-pointer hover:bg-black hover:text-white flex justify-center items-center h-8 w-8" />
-            </div>
-            <Separator orientation="vertical" className="mr-2 h-4" />
-          </SidebarProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="flex justify-between items-start h-[4rem] w-full pt-2 shadow-xl">
+          <div>
+            <SidebarProvider>
+              <div className="flex items-start">
+                <SidebarPage />
+                <SidebarTrigger className="-mr-4 cursor-pointer flex justify-center items-center h-8 w-8" />
+              </div>
+              <Separator orientation="vertical" className="mr-2 h-4" />
+            </SidebarProvider>
+          </div>
+          <div className="w-full flex flex-col">
+            <Header />
+            <main className="mt-6">
+              <Outlet />
+            </main>
+          </div>
         </div>
-        <div className="w-full flex flex-col">
-          <Header />
-          <main className="mt-6">
-            <Outlet />
-          </main>
-        </div>
-      </div>
+      </ThemeProvider>
     </section>
   );
 }
