9d048fef7a562b316c5bf37b5c1c61d0dee45ff9
Amarelshrief <amarelshrief511@gmail.com>
2025-03-26 01:45:01 +0200

    Edit HTML tag

--- src/pages/ManageTeam.tsx
+++ src/pages/ManageTeam.tsx
@@ -2,8 +2,8 @@ import TableCompo from "@/components/custom/Table/TableCompo";
 
 export default function ManageTeamPage() {
   return (
-    <>
+    <section>
       <TableCompo />
-    </>
+    </section>
   );
 }
